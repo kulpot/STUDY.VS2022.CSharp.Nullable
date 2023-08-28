08146f1a3dcf30840efa1e940f5de8219967e1b4
Kulpot <kulpotwahu@gmail.com>
2023-08-28 17:28:54 +0800

    Add project files.

---
+++ Nullable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=id6HpbQ5gek&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=19
+//  Dissecting Nullables
+
+class MainClass
+{
+    static void Main()
+    {   // int? -- value range -- int.MinValue, int.MaxValue, int.NullValue
+        //Nullable<int> j = null; // int? j = null;
+        int? i = 5;
+        //object? u = "asfdsdfNULLisValueType"; // error object is reference type must be value type
+        //int? j = 6;
+        int? j = null;  // 5 + null = null
+        int? sum = i + j;
+        Console.WriteLine(sum);
+    }
+}
